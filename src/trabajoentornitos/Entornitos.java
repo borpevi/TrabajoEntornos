fd5e60ec6508a2d31b714bee4152d76d0949d647
BLACKSUNV5 <BLACKSUNV5@DESKTOP-3IPT81J>
2019-05-11 11:47:07 +0200

    Comentario numero1

---
+++ src/trabajoentornitos/Entornitos.java
@@ -0,0 +1,15 @@
+package trabajoentornitos;
+
+public class Entornitos {
+
+	public static void main(String[] args) {
+	
+	
+		
+		System.out.println("Tres tristes tigres comen trigo en un trigal");
+		
+		
+		
+	}
+
+}
